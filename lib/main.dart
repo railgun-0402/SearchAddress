e3b03ac4b7953e5454fbe6346b4861a66de9b354
railgun-0402 <april-life.f-441-2011@docomo.ne.jp>
2023-01-20 13:40:58 +0900

    APIを実行し、住所を取得するメソッドを追加
    ■修正内容
    以下処理を追加
    ・UIからパラメータ(郵便番号)を取得
    ・郵便番号APIを実行
    ・住所用のTextに住所を反映

--- lib/main.dart
+++ lib/main.dart
@@ -29,8 +29,14 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  final postController = TextEditingController();
+  final addressController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
+    // 郵便番号
+    String postNum = '';
+
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
@@ -50,13 +56,19 @@ class _MyHomePageState extends State<MyHomePage> {
                     width: 90),
                 Expanded(
                   child: TextFormField(
+                    controller: postController,
                     decoration: InputDecoration(labelText: '郵便番号を入力'),
+                    onChanged: (text) {
+                      // textFieldの値を取得
+                      postNum = postController.text;
+                    },
                   ),
                 ),
                 OutlinedButton(
                   child: Text('検索'),
                   onPressed: () {
-                    searchAddress('0010001');
+                    // 郵便番号で住所を検索するAPIを実行
+                    searchAddress(postNum);
                   },
                 ),
               ],
@@ -71,6 +83,7 @@ class _MyHomePageState extends State<MyHomePage> {
                     width: 90),
                 Expanded(
                   child: TextFormField(
+                    controller: addressController,
                     decoration: InputDecoration(),
                   ),
                 ),
@@ -82,14 +95,27 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 
+  // APIを実行して、住所を探す
+  // またテキストに得られた住所を書き出す
   Future<void> searchAddress(String zipCode) async {
     // URL
-    String url = 'https://zipcloud.ibsnet.co.jp/api/search?zipcode=$zipCode';
+    String url = 'https://zipcloud.ibsnet.co.jp/api/search?zipcode=';
+    // 住所
+    String address = '';
+
+    // URLのクエリ文字に郵便番号を追記
+    url = url + zipCode;
+    // 実行
     var result = await get(Uri.parse(url));
 
     // json形式(key/value)なのでMapを使用
     Map<String, dynamic> data = jsonDecode(result.body);
-    // 後は呼び出し元に返却でも出力でも任意の操作で！
-    print(data);
+
+    // それぞれの要素から住所を取得
+    address = data['results'][0]['address1'];
+    address += data['results'][0]['address2'];
+    address += data['results'][0]['address3'];
+
+    addressController.text = address;
   }
 }
